cf8338c7f51b0b3ace6d56a8bb3d23724383cc7a
ntwari egide <ntwariegide2@gmail.com>
2021-10-23 11:16:37 -0700

    ft(score-model): implement score model

---
+++ model/score.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose")
+const bcrypt = require("bcryptjs")
+
+let ScoreSchema = mongoose.Schema({
+    score: {
+        type: Number,
+        require: 'Score amount is required'
+    },
+
+    level: {
+        type: Number,
+        require: 'Level is required'
+    },
+    speed: {
+        type: String,
+        require: 'Speed is required'
+    },
+
+    playedAt: { type: Date, default: Date.now }
+})
+
+const Score = mongoose.model("scores",ScoreSchema)
+module.exports = Score
