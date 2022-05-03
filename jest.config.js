328087797b76fbfd13d9b99036816b1e29f98cb2
ntwari-egide <ntwariegide2@gmail.com>
2022-05-03 22:04:26 +0200

    ft(tests): implement add tests

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+// jest.config.js
+module.exports = {
+    // setupTestFrameworkScriptFile has been deprecated in
+    // favor of setupFilesAfterEnv in jest 24
+    // setupFilesAfterEnv: ['./jest.setup.js'],
+    testTimeout: 80000
+  }
+  
--- tests/score.test.js
+++ tests/score.test.js
@@ -1,16 +1,66 @@
 const request = require('supertest')
 const assert = require('assert')
 const app = require('../app')
+const { response } = require('../app')
 
-describe('GET /api/v1/scores', () => {
-    it('get all scores ', (done ) => {
-        request(app)
+describe('/api/v1/scores', () => {
+
+    it('get all scores ', async ( ) => {
+
+        await request(app)
             .get('/api/v1/scores')
             .set('Accept', 'application/json')
             .expect(200)
-            .end( (err, res) => {
-                if(err) return done(err);
-                return done()
-            })
     })
+
+    it('add new score', async () => {
+
+        await request(app)
+            .post('/api/v1/scores')
+            .send({score: 10, level: 2,speed: "FAST"})
+            .expect('Content-Type', /json/)
+            .expect(200)
+    })
+
+})
+
+describe('score statistics', () => {
+    it('GET /get-top-10 ', async () => {
+
+        await request(app)
+            .get('/api/v1/scores/get-top-10')
+            .set('Accept', 'application/json')
+            .expect(200)
+    })
+
+    it('GET /get-top-10 return length 10', async () => {
+
+        await request(app)
+            .get('/api/v1/scores/get-top-10')
+            .set('Accept', 'application/json')
+            .expect(200)
+            .then( response => {
+                assert(response.data.length, 10)
+            })
+
+    })
+
+    it('GET /score-statistics ', async () => {
+
+        await request(app)
+            .get('/api/v1/scores/get-score-statistics')
+            .set('Accept','application/json')
+            .expect(200)
+    })
+
+    it('GET /score-statistics return max gtr 0', async () => {
+        
+        await request(app)
+            .get('/api/v1/scores/get-score-statistics')
+            .set('Accept','application/json')
+            .expect(200)
+            .then( response => {
+                assert(response.data.length, 1)
+            })
+    } )
 })
